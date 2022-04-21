3e91e87b596d63486678dbd8442cbed38d52d323
THE_ORONCO <the_oronco@posteo.net>
2022-04-21 20:25:55 +0200

    changed bot.py back

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
+import asyncio
 from discord import Game
 from discord.ext import commands
+from discord.ext.commands import Bot
+from discord.utils import get
 import json
 import os
 import traceback
@@ -15,9 +18,7 @@ PREFIX = config['prefix']
 COGS_DIR = "cogs"
 
 # define bot
-intents = discord.Intents().all()
-client = commands.Bot(command_prefix=PREFIX, intents=intents)
-
+client = commands.Bot(command_prefix=PREFIX)
 
 # events
 @client.event
@@ -25,33 +26,40 @@ async def on_ready():
     await client.change_presence(activity=Game(name="a CTF!"))
     print(f"Logged in as {client.user.name} ({client.user.id})")
 
-
 @client.event
 async def on_command_error(ctx, error):
     error = getattr(error, 'original', error)
 
-    match error:
-        case commands.CommandNotFound():
-            return
-        case commands.DisabledCommand():
-            await ctx.send(f"{ctx.command} has been disabled.")
-        case commands.NoPrivateMessage():
-            await ctx.author.send(f"{ctx.command} can not be used in Private Messages.")
-        case commands.MissingRequiredArgument():
-            await ctx.send(f"Missing argument: {error.param}")
-        case commands.ChannelNotFound():
-            await ctx.send(f"Channel or Category not found: {error.argument}")
-        case _:
-            print('Unhandled exception in command {}:'.format(ctx.command), file=sys.stderr)
-            traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+    ignored = (commands.CommandNotFound, )
+    if isinstance(error, ignored):
+        return
 
+    if isinstance(error, commands.DisabledCommand):
+        await ctx.send(f"{ctx.command} has been disabled.")
 
-for extension in [f"{COGS_DIR}.{x[:-len('.py')]}" for x in os.listdir(COGS_DIR) if x.endswith(".py")]:
-    try:
-        client.load_extension(extension)
-        print('Loaded extension \"{}\"'.format(extension))
-    except Exception as e:
-        exc = '{}: {}'.format(type(e).__name__, e)
-        print('Failed to load extension \"{}\"\n{}'.format(extension, exc))
+    elif isinstance(error, commands.NoPrivateMessage):
+        await ctx.author.send(f"{ctx.command} can not be used in Private Messages.")
+
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"Missing argument: {error.param}")
+
+    elif isinstance(error, commands.ChannelNotFound):
+        await ctx.send(f"Channel not found: {error.argument}")
+
+    else:
+        print('Unhandled exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+
+async def main():
+    for extension in [f"{COGS_DIR}.{x[:-len('.py')]}" for x in os.listdir(COGS_DIR) if x.endswith(".py")]:
+        try:
+            await client.load_extension(extension)
+            print('Loaded extension \"{}\"'.format(extension))
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension \"{}\"\n{}'.format(extension, exc))
+
+if __name__ == "__main__":
+    asyncio.run(main())
 
 client.run(TOKEN)
